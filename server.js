4262888da4c12790735b4780cda004ce9e056f5c
Bird_GHB_PC <bird_aries@hotmail.com>
2020-08-15 15:50:54 +0700

    Return Product Code 0.1

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.listen(app.get('port'), function(){
 })
 
 /* webhook */
-app.post('/webhook',(req,res) => {
+app.post('/webhooks',(req,res) => {
 
   console.log(req.body.events[0].message);
 
@@ -31,12 +31,129 @@ app.post('/webhook',(req,res) => {
   res.send('Hi');
 })
 
+/* INQ Product*/
+app.post('/webhook',(req,res) => {
+  let msg = req.body.events[0].message;
+  let replyToken = req.body.events[0].replyToken;
+
+  let userIDToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU5OTk5NzAyN30.mF00sfcPgRdoqTyjOf3DVzVXRuKPBm0axyz-P0FT1g2-c5SIXWqj00FVwNT5D2IDRFfEXjLw7VQuPd63gosKzw";
+  if( msg = "Product" ){
+    let userProduct = getProduct(userIDToken );
+
+    console.log(userProduct);
+
+  }
+
+})  
+
+function  getProduct(userIDToken){
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization' : 'Bearer '+ userIDToken
+  };
+  let body = JSON.stringify({token: "0a8ae57106f361d288e48f5c63ef9e7f"});
+  //let jsonObj ;
+  
+  /*
+  request.post({
+    url:'http://apitest.ghb.co.th/api/openSaving/getParameters',
+    headers: headers,
+    body:body
+  },(err,res,body) => {
+    if(res.statusCode == 200){
+      jsonObj = JSON.parse(res.body);
+      console.log('success');
+    }else{
+      jsonObj = null;
+    }
+  })*/
+  
+  request.post({
+    url:'http://apitest.ghb.co.th/api/openSaving/getParameters',
+    headers: headers,
+    body: body
+  },(err,res,body) => {
+    if(res.statusCode == 200){
+      console.log('success');
+      return JSON.parse(res.body);
+    }
+  })
+  
+}
+
+
+
+/* API OpenAccount */
+app.get('/getParameters',(req,res) => {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization' : 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU5OTk5NzAyN30.mF00sfcPgRdoqTyjOf3DVzVXRuKPBm0axyz-P0FT1g2-c5SIXWqj00FVwNT5D2IDRFfEXjLw7VQuPd63gosKzw'
+  }
+  let body1 = JSON.stringify({
+    token: "0a8ae57106f361d288e48f5c63ef9e7f"
+  })
+  
+
+  request.post({
+    url:'http://apitest.ghb.co.th/api/openSaving/getParameters',
+    headers: headers,
+    body: body1
+  },(err,res,body1) => {
+    if(res.statusCode == 200){
+      console.log('success');
+      let jsonObj = JSON.parse(res.body);
+      LinePushProduct(jsonObj.data.product_list);
+    }else{
+      console.log('err =' + err)
+      console.log('status ='+ res.statusCode)
+    }
+  })
+})
+
+
+function LinePushProduct(productList)
+{
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization' : 'Bearer 1/B0JKb0SkxFfgDOBy1T+DeXfWwypgAc6SI00dT92fb2VGTGw92YrtpkP6O1P5uuz1F6K/2sECRTxbbKjDyJm/9pVDzc79VAJAfg2esirZ7RhlCzEQALWMqBuYj16UHxAtpL3E7+kwISr4laiwS8rwdB04t89/1O/w1cDnyilFU='
+  }
+  /*
+  let msg =  [{
+    type:"text",
+    text:"ไม่สามารถทำรายการได้"
+  },{
+    type:"text",
+    text:"ไม่สามารถทำรายการได้"
+  }]
+  */
+  let msgEx = [] ;
+
+  productList.forEach(i => {
+    msgEx.push({
+      type:"text",
+      text:i.deposit_type_code
+    })
+  });
+  
+  request.post({
+    url:'https://api.line.me/v2/bot/message/push',
+    headers: headers,
+    body: JSON.stringify({
+      to: "Ub41490a9e6b2bc5f128abce7c2f921e1",
+      messages:msgEx
+    })
+  })
+
+}
+
 function reply(replyToken,msg){
 
   let headers = {
     'Content-Type': 'application/json',
     'Authorization' : 'Bearer 1/B0JKb0SkxFfgDOBy1T+DeXfWwypgAc6SI00dT92fb2VGTGw92YrtpkP6O1P5uuz1F6K/2sECRTxbbKjDyJm/9pVDzc79VAJAfg2esirZ7RhlCzEQALWMqBuYj16UHxAtpL3E7+kwISr4laiwS8rwdB04t89/1O/w1cDnyilFU='
   }
+
+  
 
   if(msg.type == 'sticker'){
     var body  = JSON.stringify({
